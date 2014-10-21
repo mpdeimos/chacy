cef4f99d2c2bb899315c822e98f5444758a7c981
Martin Pöhlmann <mpdeimos@users.noreply.github.com>
2014-10-21 21:06:59 +0200

    started rewrite of harness to test suite

---
+++ src/test/java/com/mpdeimos/chacy/CompleteHarnessSuite.java
@@ -0,0 +1,51 @@
+package com.mpdeimos.chacy;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Tests the harness directory. TODO change this to be a test suite.
+ */
+@RunWith(Parameterized.class)
+public class CompleteHarnessSuite extends HarnessTestBase
+{
+	/** Name of the simple test harness. */
+	private static final String SIMPLE = "simple";
+
+	/** The harness to test. */
+	private final String harness;
+
+	/** @return the test harnesses. */
+	@Parameters(name = "{0}")
+	public static Iterable<String[]> getHarnesses()
+	{
+		return Arrays.asList(new String[][] { { SIMPLE } });
+	}
+
+	/** Constructor. */
+	public CompleteHarnessSuite(String harness)
+	{
+		this.harness = harness;
+	}
+
+	/** Tests the simple harness. */
+	@Test
+	public void testHarness() throws IOException
+	{
+		assertHarness(this.harness);
+	}
+
+	/** tests the given harness. */
+	private void assertHarness(String harness) throws IOException
+	{
+		CompilationResult result = getHarness(harness).compileFiles();
+
+		result.assertSuccess();
+		result.assertOutput();
+	}
+}
